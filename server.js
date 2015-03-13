570433b315849f140ad9bf4affb19f5d97f3e384
Nikita Cherevkov <nikita.cherevkov@gmail.com>
2015-03-13 09:39:10 -0400

    playing with port

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.get('*', function(req, res){
 	});
 });
 
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
