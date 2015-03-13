307f8627e78b7abaf21765b5f4e0667b1332ce66
Nikita Cherevkov <nikita.cherevkov@gmail.com>
2015-03-13 16:39:36 -0400

    created 'User' schema

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,21 @@ module.exports = function(config){
 	db.once('open', function callback(){
 		console.log('multivision db opened');
 	});
+
+
+	var userSchema = mongoose.Schema({
+		firstName: String,
+		lastName: String,
+		username: String
+	});
+
+	var User = mongoose.model('User', userSchema);
+
+	User.find({}).exec(function(err, collection){
+		if(collection.length === 0) {
+			User.create({firstName:'Nikita', lastName: 'Cherevkov', username: 'n1k1ch'});
+			User.create({firstName:'Alexandr', lastName: 'Lavrov', username: 'hahol1989'});
+			User.create({firstName:'Sergey', lastName: 'Trindiuk', username: 'serega_batya1987'});
+		}
+	});
 }
