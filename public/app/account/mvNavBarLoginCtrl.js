7f8705a6ac3378c3a2ec735deefddc0cd5c54aa1
Nikita Cherevkov <nikita.cherevkov@gmail.com>
2015-03-13 16:58:26 -0400

    client-side logIn, part 1

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,12 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope){
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http){
 	$scope.signin = function(username, password) {
-		console.log('Not implemented yet');
+		$http.post('/login', {username: username, password: password})
+			.then(function(response){
+				if(response.data.success) {
+					console.log('logged in!');
+				} else {
+					console.log('failed to log in (');
+				}
+		});
 	}
 });
