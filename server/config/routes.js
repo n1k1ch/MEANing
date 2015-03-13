e2656ae3f32acde61b4d7414c74c00816f6f62af
Nikita Cherevkov <nikita.cherevkov@gmail.com>
2015-03-13 16:52:37 -0400

    added server-side login code

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,32 @@
+var passport = require('passport');
+
 module.exports = function(app){
 	app.get('/partials/*', function(req, res){
 		//console.log('getting /partials/' + req.params.partialPath );
 		res.render('../../public/app/' + req.params[0]);
+	});
+
+
+	app.post('/login', function(req, res, next) {
+		var auth = passport.authenticate('local', function(err, user) {
+			if(err) {
+				return next(err);
+			}
+			
+			if(!user) {
+				res.send({success:false});
+			}
+
+			req.logIn(user, function(err) {
+				if(err) {
+					return next(err);
+				}
+
+				res.send({success: true, user: user});
+			});
+		});
+
+		auth(req, res, next);
 	});
 
 
