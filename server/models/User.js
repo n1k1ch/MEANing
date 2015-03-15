843706095ed14166a469766865f3d3a7b8f9c98f
n1k1ch <nikita.cherevkov@gmail.com>
2015-03-15 23:13:27 +0200

    Signup: Implementing Model Validation

--- server/models/User.js
+++ server/models/User.js
@@ -2,19 +2,23 @@ var mongoose = require('mongoose'),
 	encrypt = require('../utilities/encryption');
 
 var userSchema = mongoose.Schema({
-	firstName: String,
-	lastName: String,
-	username: String,
-	salt: String,
-	hashed_pwd: String,
-	roles: [String]
+	firstName: {type:String, required: '{PATH} is required!'},
+	 lastName: {type:String, required: '{PATH} is required!'},
+	 username: {
+		 type: String,
+		 required: '{PATH} is required!',
+		 unique: true//with this line, mongoose 3.9.x falls with "cannot read property 'length' of undefined at processResults". Switched to mongoose 3.8.25
+	 },
+	 salt: {type:String, required: '{PATH} is required!'},
+	 hashed_pwd: {type:String, required: '{PATH} is required!'},
+	 roles: [String]
 });
 
 userSchema.methods = {
 	authenticate: function(passwordToMatch) {
 		return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
 	}
-}
+};
 
 var User = mongoose.model('User', userSchema);
 
@@ -26,7 +30,7 @@ function createDefaultUsers() {
 			createSaltedUser('Sergey', 'Trindiuk', 'serega_batya1987', User, []);
 		}
 	});
-}
+};
 
 function createSaltedUser(firstName, lastName, username, User, roles) {
 	var salt = encrypt.createSalt();
@@ -37,6 +41,6 @@ function createSaltedUser(firstName, lastName, username, User, roles) {
 		hashed_pwd: encrypt.hashPwd(salt, username),
 		roles: roles
 	});
-}
+};
 
 exports.createDefaultUsers = createDefaultUsers;
