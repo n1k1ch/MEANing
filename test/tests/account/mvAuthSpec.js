156472671ee9e1e75d7ad2aa1604540694c98bca
n1k1ch <nikita.cherevkov@gmail.com>
2015-03-15 22:25:51 +0200

    not added tests for mvAuth.js, disabled karma coverage

---
+++ test/tests/account/mvAuthSpec.js
@@ -0,0 +1,33 @@
+/**
+ * Created by n1k1ch on 15.03.2015.
+ */
+/*TODO: implement
+describe('mvAuth', function(){
+
+    var mockHttp;
+
+
+    beforeEach(function() {
+        module('app');
+
+        mockHttp = sinon.stub({post: function(str, resp){
+            console.log('calling stub post' );
+            return {some: 'sssome'};
+        }
+        });
+
+        module(function($provide) {
+            $provide.value('$http', mockHttp);
+        });
+    });
+
+    describe('authenticateUser', function() {
+
+        it('should return false', inject(function(mvAuth) {
+            expect(mvAuth.authenticateUser('alala', 'shalala'));//.should.eventually.equal(false);
+        }));
+
+    });
+
+});
+    */
